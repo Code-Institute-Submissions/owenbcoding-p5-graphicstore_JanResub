3970c804920e36c22d45d4822139c68784799748
eoghankb <eoghankb@gmail.com>
2023-01-16 23:53:20 +0000

    product admin - product form

--- products/forms.py
+++ products/forms.py
@@ -1,4 +1,4 @@
-from .models import Comment
+from .models import Comment, Product, Category
 from django import forms
 
 
@@ -9,3 +9,22 @@ class CommentForm(forms.ModelForm):
         labels = {
             'body': '',
         }
+
+
+class ProductForm(form.Modelform):
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            categories = Category.objects.all()
+            friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+
+            self.fields['category'].choices = friendly_names
+            for field_name, field in self.fields.items():
+                field.widget.attrs['class'] = 'border-black rounded-0'
+            
+            
+ 
