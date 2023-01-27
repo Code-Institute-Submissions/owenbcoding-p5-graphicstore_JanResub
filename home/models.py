d8dbe07c0b3e3e89d8bcdf0238a49ddeec9bfd97
eoghankb <eoghankb@gmail.com>
2023-01-27 08:40:53 +0000

    added userid to newsletter model and imported user

--- home/models.py
+++ home/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Custom model number 1
+# Custom model newsletter 1
 
 
 class Newsletter(models.Model):
     email = models.EmailField(max_length=254, null=False, blank=False)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     date = models.DateTimeField(auto_now_add=True)
