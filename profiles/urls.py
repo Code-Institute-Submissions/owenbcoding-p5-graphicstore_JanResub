565a2416d27e1980e3e53193c45a99a4188ca1fd
eoghankb <eoghankb@gmail.com>
2023-01-27 22:42:18 +0000

    added ticket delete url

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('profile/', views.profile, name='profile'),
+    path('', views.profile, name='profile'),
     path('order_history/<order_number>',
          views.order_history,
          name='order_history'),
     path('tickets/', views.tickets, name='tickets'),
     path("newsletter/update", views.editemail, name="editemail"),
+    path("tickets/<int:ticket_id>", views.ticketdetail, name="ticket_detail"),
+    path("tickets/<int:ticket_id>/delete", views.ticketdelete, name="ticket_delete"),
 ]
