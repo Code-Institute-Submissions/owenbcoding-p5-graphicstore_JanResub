99ad96c5b43c40f4745f1035c61ed7ecf4fba213
eoghankb <eoghankb@gmail.com>
2023-01-27 09:04:34 +0000

    added function for unsubscribe and save user id to subscribe

--- home/views.py
+++ home/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django import forms
 from .forms import NewsletterForm
-from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404, redirect, render, reverse
+from .models import Newsletter
+from django.http import HttpResponseRedirect
+
 
 
 def index(request): 
@@ -20,8 +23,28 @@ def about(request):
 
 
 def subscribe(request):
-    """ """
+    """ Part of Custom model Crud for creating a newsletter by subscribing """
     form = NewsletterForm(request.POST)
     if form.is_valid():
+        form = form.save(commit=False)
+        if request.user.is_authenticated:
+            form.user_id = request.user
+        else:
+            form.user_id = None
         form.save()
     return redirect('index')
+
+
+def unsubscribe(request):
+    """Part of Custom model Crud for news letter A user or guest can ubsubscribe / delete their email from updates"""
+    form = NewsletterForm(request.POST)
+    if request.user.is_authenticated:
+        newsletter = Newsletter.objects.get(user_id=request.user)
+        newsletter.delete()
+    else:
+        if form.is_valid():
+            newsletter = Newsletter.objects.get(
+                email=form.cleaned_data["email"]
+            )
+            newsletter.delete()
+    return HttpResponseRedirect(reverse("index"))
